9140b7497c968eec63ef8f0866dc08f76d638c19
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-01-10 21:25:28 -0600

    Component setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Header from './Components/Header/Header'
+import App from './Components/App/App';
+import Footer from './Components/Footer/Footer'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header />
     <App />
+    <Footer />
   </React.StrictMode>
 );
 

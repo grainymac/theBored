5593cd10f28bdf1a630ba8c5cd234740d87782e2
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-01-12 10:39:02 -0600

    Completed build for header for now

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,4 @@
-import React, { useState, useRef } from 'react'
-import { Link } from 'react-router-dom';
-
-import { gsap } from 'gsap'
-import { ExpoScaleEase } from "gsap/EasePack"
+import React from 'react'
 
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -11,47 +7,25 @@ import AppBar from '@mui/material/AppBar'
 
 import './Header.css'
 
-gsap.registerPlugin(ExpoScaleEase);
-const navigation = [
-    { path: '/', text: 'Home' },
-];
-
-const ITEM_HEIGHT = 48;
-
 const Header = () => {
-    let menu = useRef(null)
-    let links = useRef(null)
-
-    let tl = gsap.timeline({ paused: true });
-
-    tl.to(menu, { duration: 1, opacity: 1, height: '60vh', ease: 'expo.inOut' })
-    tl.from(links, { duration: 1,  opacity: 0, y: 20, stagger: 0.1, ease: 'expo.inOut', }, "-=0.5");
-    tl.reverse();
-            //   <Typography variant="h2" className='links' component="h2">
-            //     home
-            //   </Typography>
-            //   <Typography variant="h2" className='links' component="h2">
-            //     welcome    
-            //   </Typography>
-            //   <Typography variant="h2" className='links' component="h3">
-            //     the board
-            //   </Typography>
 
     return (
         <Box className='header-container' sx={{ flexGrow: 1 }}>
-          <AppBar position="static">
-            <Toolbar className='toolbar' disableGutters>
-                <div>Logo</div>
-                <div class="ham" onClick={() => tl.reversed(!tl.reversed)}>
-                    <span>☰</span>
-                </div>
-            </Toolbar>
-          </AppBar>
+            <AppBar position="static">
+                <Toolbar className='toolbar' disableGutters>
+                    <Typography variant="h2" className='links' component="h2">
+                        home
+                    </Typography>
+                    <Typography variant="h2" className='links' component="h2">
+                        welcome    
+                    </Typography>
+                    <Typography variant="h2" className='links' component="h3">
+                        the board
+                    </Typography>
+                </Toolbar>
+            </AppBar>
         </Box>
-      );
-    
-
-
+    );
 }
 
 export default Header

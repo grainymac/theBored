e589e3d2637391d0602bf9f9f35b0161098b39fd
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-01-12 12:21:12 -0600

    finished building structure

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
+import boredLogo from '../../assets/theBored-blackRed.png'
 import './App.css';
+
+import InfoCard from '../InfoCard/InfoCard'
+import TaskCard from '../TaskCard/TaskCard'
 
 const App = () => {
   const [tasks, setTasks] = useState({})
@@ -30,11 +34,19 @@ const App = () => {
 
   return (
     <main className='main-container'>
-      <h1>theBored</h1>
-      <p>
-        {tasks.activity}
-      </p>
-      <button onClick={() => getAllTasks()}>click it</button>
+      <div className='logo-box'>
+        <img src={boredLogo} className='bored-logo' />
+      </div>
+      <div className='info-box'>
+        <InfoCard />
+      </div>
+      <div className='task-box'>
+        <TaskCard tasks={tasks.activity} />
+      </div>
+      <div className='button-box'>
+        <button onClick={() => getAllTasks()}>click it</button>
+        <button onClick={() => getAllTasks()}>click it</button>
+      </div>
     </main>
   );
 }

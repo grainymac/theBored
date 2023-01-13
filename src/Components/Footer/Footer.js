7a9fa0396ea8dc134b2cdd92adada7227bf06d62
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-01-12 21:09:56 -0600

    Finished badurl page structure

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import './Footer.css'
 
-const Footer = () => {
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import AppBar from '@mui/material/AppBar'
 
+const Footer = () => {
+    return (
+        <Box className='header-container' sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar className='toolbar' disableGutters>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    )
 }
 
 export default Footer

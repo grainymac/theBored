0bb7c56fdf2c67f703f8e30ec912b69e4681cc47
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-01-15 09:25:46 -0600

    Styling for activity card

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -8,7 +8,7 @@ const Welcome = () => {
             <h1 className='welcome-title'>Welcome to theBored</h1>
             <div className='welcome-txt'>
                 <p>
-                    We are pleased to welcome you to The Bored. The purpose of this application is to help you discover something new to do. Instead of thinking of things to do while you're bored, this application will generate an activity for you at random. Then, you can save it and it will appear on your board.
+                    I am pleased to welcome you to The Bored. The purpose of this application is to help you discover something new to do. Instead of thinking of things to do while you're bored, this application will generate an activity for you at random. Then, you can save it and it will appear on your board.
                 </p>
                 <p>
                     On your board, you can see all the activities that you have saved. If you decide that you don't want to do an activity, you can now either delete it from your board or mark it as done on your board after you've completed it. It will stay on your board so you don't have to remember if you've completed that activity or not.
